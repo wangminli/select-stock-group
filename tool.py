22a53cf221c90a9a686472fcc59886da13569967
wangminli1 <wangminli@jd.com>
2025-08-20 14:44:22 +0800

    量化策略 https://bbs.quantclass.cn/thread/64336

--- tool.py
+++ tool.py
@@ -6,10 +6,7 @@ if __name__ == '__main__':
     mode = input("请输入模式(1: 查看策略, 2: 查看因子): ")
     backtest_config = load_config()
     if mode == '1':
-        plot_stock_kline(backtest_config, k_start='2018-01-01', k_end='2025-06-01', add_days=10)
+        plot_stock_kline(backtest_config, k_start='2009-01-01', k_end='2025-06-01', add_days=10)
     elif mode == '2':
-        name = '市值_None'
-        factor_analysis(backtest_config, factor_name=name)
-    elif mode == '3':
-        name = '市值_None'
+        name = 'factor_20250801_24'
         factor_analysis(backtest_config, factor_name=name)
